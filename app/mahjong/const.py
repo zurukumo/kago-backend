37a0283f43b7c41f0e577804cf24b654e716da22
zurukumo <zurukumo@gmail.com>
2021-06-30 23:12:37 +0900

    refactor: ステートを表す定数を文字化

--- app/mahjong/const.py
+++ app/mahjong/const.py
@@ -5,17 +5,17 @@ class Const:
     AUTO_MODE = 2
 
     # ステート
-    INITIAL_STATE = -100
-    KYOKU_START_STATE = -10
-    TSUMO_STATE = 0
+    INITIAL_STATE = 'INITIAL'
+    KYOKU_START_STATE = 'KYOKUSTART'
+    TSUMO_STATE = 'TSUMO'
     # NOTICE1 - リーチ/暗槓/加槓
-    NOTICE1_STATE = 10
-    DAHAI_STATE = 20
+    NOTICE1_STATE = 'NOTICE1'
+    DAHAI_STATE = 'DAHAI'
     # NOTICE2 - 明槓/ポン/チ
-    NOTICE2_STATE = 30
-    AGARI_STATE = 40
-    RYUKYOKU_STATE = 50
-    SYUKYOKU_STATE = 60
+    NOTICE2_STATE = 'NOTICE2'
+    AGARI_STATE = 'AGARI'
+    RYUKYOKU_STATE = 'RYUKYOKU'
+    SYUKYOKU_STATE = 'SYUKYOKU'
 
     def __init__(self):
         pass
